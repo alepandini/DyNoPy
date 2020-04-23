b8e33d8e743817455c178f9321aa6b195ceb5a8b
Sarath C Dantu <sarathchandradantu@gmail.com>
2020-04-23 10:21:25 +0100

    v0.2 license corrections

--- bin/dyno_paire.py
+++ bin/dyno_paire.py
@@ -4,24 +4,18 @@
     @version       : $release_version
     @author        : Sarath Chandra Dantu
     
-
      Copyright (C) 2020 Sarath Chandra Dantu & Alessandro Pandini
-
      This file is part of: Dynamics based Network cOmparisons in Python (DyNoPy)
-
      DyNoPy is a free software: you can redistribute it and/or modify
      it under the terms of the GNU General Public License as published by
      the Free Software Foundation, either version 3 of the License, or
      (at your option) any later version.
-
-     DPD is distributed in the hope that it will be useful,
+     DyNoPy is distributed in the hope that it will be useful,
      but WITHOUT ANY WARRANTY; without even the implied warranty of
      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
      GNU General Public License for more details.
-
      You should have received a copy of the GNU General Public License
      along with DyNoPy.  If not, see <http://www.gnu.org/licenses/>.
-
 '''
 import timeit,os,sys
 import multiprocessing as mp
--- dynoIO/options_optparse.py
+++ dynoIO/options_optparse.py
@@ -5,26 +5,28 @@
     @author        : Sarath Chandra Dantu
     
 
-     Copyright (C) 2018-2019 Sarath Chandra Dantu & Alessandro Pandini
+     Copyright (C) 2020 Sarath Chandra Dantu & Alessandro Pandini
 
-     This file is part of: Design of Protein Dynamics package (DPD)
+     This file is part of: Dynamics based Network cOmparisons in Python (DyNoPy)
 
-     DPD is a free software: you can redistribute it and/or modify
+     DyNoPy is a free software: you can redistribute it and/or modify
      it under the terms of the GNU General Public License as published by
      the Free Software Foundation, either version 3 of the License, or
      (at your option) any later version.
 
-     DPD is distributed in the hope that it will be useful,
+     DyNoPy is distributed in the hope that it will be useful,
      but WITHOUT ANY WARRANTY; without even the implied warranty of
      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
      GNU General Public License for more details.
 
      You should have received a copy of the GNU General Public License
-     along with DPD.  If not, see <http://www.gnu.org/licenses/>.
+     along with DyNoPy.  If not, see <http://www.gnu.org/licenses/>.
 
 '''
 from optparse import OptionParser
-def dpd_list_archer():
+print('IMPORTING_BAD_CODE options_optparser.py. EXITING')
+exit()
+def _list_archer():
     global logger
     out_usage ="dpd_list_simulations runs equilibration and generates files for archer.\n\n\n"
     out_usage+="DPD_Test.py -s list_of_mutations -l Run-1 -c cut_off -v 1 \n"
